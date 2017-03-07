2e8a8dde9a1f937b15a13bfba2e1ac0f39014406
Pete Muir <pmuir@bleepbleep.org.uk>
2017-03-06 20:48:27 -0500

    chore(build): cleanup the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,6 @@
 
 var gulp = require('gulp'),
-  runSequence = require('run-sequence'),
-  del = require('del'),
-  replace = require('gulp-string-replace'),
   sourcemaps = require('gulp-sourcemaps'),
-  exec = require('child_process').exec,
   ngc = require('gulp-ngc'),
   changed = require('gulp-changed');
 
@@ -17,13 +13,6 @@ var globalExcludes = [ '!./**/examples/**', '!./**/examples' ]
  * FUNCTION LIBRARY
  */
 
-function copyToDist(srcArr) {
-  return gulp.src(srcArr.concat(globalExcludes))
-    .pipe(gulp.dest(function (file) {
-      return libraryDist + file.base.slice(__dirname.length); // save directly to dist
-    }));
-}
-
 function updateWatchDist() {
   return gulp
     .src([libraryDist + '/**'].concat(globalExcludes))
@@ -35,40 +24,14 @@ function updateWatchDist() {
  * TASKS
  */
 
-gulp.task('post-transpile', ['transpile'], function () {
-  return gulp.src(['dist/src/app/**/*.js'])
-    .pipe(replace(/templateUrl:\s/g, "template: require("))
-    .pipe(replace(/\.html',/g, ".html'),"))
-    .pipe(replace(/\.html'/g, ".html')"))
-    .pipe(replace(/styleUrls: \[/g, "styles: [require("))
-    .pipe(replace(/\.scss']/g, ".css').toString()]"))
-    .pipe(gulp.dest(function (file) {
-      return file.base; // because of Angular 2's encapsulation, it's natural to save the css where the scss-file was
-    }));
-});
-
-//Sass compilation and minifiction
-gulp.task('transpile-sass', function () {
-  return transpileSASS(appSrc + '/app/**/*.scss');
-});
-
-// Put the SASS files back to normal
 gulp.task('build-library',
   [
     'transpile',
-    'post-transpile',
-    'copy-html',
     'copy-static-assets'
   ]);
 
 gulp.task('transpile', function () {
   return ngc('tsconfig.json')
-});
-
-gulp.task('copy-html', function () {
-  return copyToDist([
-    'src/**/*.html'
-  ]);
 });
 
 gulp.task('copy-static-assets', function () {
@@ -89,17 +52,7 @@ gulp.task('copy-watch-all', ['build-library'], function() {
 });
 
 gulp.task('watch', ['build-library', 'copy-watch-all'], function () {
-  gulp.watch([appSrc + '/app/**/*.ts', '!' + appSrc + '/app/**/*.spec.ts'], ['transpile', 'post-transpile', 'copy-watch']).on('change', function (e) {
+  gulp.watch([appSrc + '/app/**/*.ts', '!' + appSrc + '/app/**/*.spec.ts'], ['transpile', 'copy-watch']).on('change', function (e) {
     console.log('TypeScript file ' + e.path + ' has been changed. Compiling.');
-  });
-  gulp.watch([appSrc + '/app/**/*.scss']).on('change', function (e) {
-    console.log(e.path + ' has been changed. Updating.');
-    transpileSASS(e.path);
-    updateWatchDist();
-  });
-  gulp.watch([appSrc + '/app/**/*.html']).on('change', function (e) {
-    console.log(e.path + ' has been changed. Updating.');
-    copyToDist(e.path);
-    updateWatchDist();
   });
 });
